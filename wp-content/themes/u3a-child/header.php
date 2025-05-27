5b746fb4e5e19e6e2d64a89d1377303736b70df6
jeomin12 <145843932+jeomin12@users.noreply.github.com>
2025-05-27 15:30:43 +1000

    Update header

--- wp-content/themes/u3a-child/header.php
+++ wp-content/themes/u3a-child/header.php
@@ -9,33 +9,35 @@
 <?php wp_body_open(); ?>
 
 <header class="u3a-header">
-  <div class="u3a-top-bar">
-    <div class="u3a-logo">
-      <a href="<?php echo home_url(); ?>">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo1.png" alt="U3A Townsville Logo">
-      </a>
-    </div>
-
-    <div class="u3a-header-right">
-      <a href="#">Contact</a>
-      <a href="#">Login</a>
-
-      <form class="u3a-search-form" method="get" action="<?php echo esc_url(home_url('/')); ?>">
-        <input type="search" name="s" placeholder="Search">
-        <button type="submit"><span class="dashicons dashicons-search"></span></button>
-      </form>
+  <div class="u3a-header-top">
+    <div class="u3a-header-inner">
+      <div class="u3a-logo">
+        <a href="<?php echo home_url(); ?>">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo1.png" alt="U3A Townsville Logo">
+        </a>
+      </div>
+      <div class="u3a-header-utilities">
+        <a href="/contact">Contact</a>
+        <a href="/login">Login</a>
+        <form class="u3a-search-form" method="get" action="<?php echo esc_url(home_url('/')); ?>">
+          <input type="search" name="s" placeholder="Search...">
+          <button type="submit"><span class="dashicons dashicons-search"></span></button>
+        </form>
+      </div>
     </div>
   </div>
 
   <nav class="u3a-navbar">
-    <?php
-      wp_nav_menu(array(
-        'theme_location' => 'primary',
-        'menu_class'     => 'u3a-menu',
-        'container'      => false,
-      ));
-    ?>
-
-    <a href="#" class="u3a-donate-btn">Donate now</a>
+    <div class="u3a-nav-inner">
+      <?php
+        wp_nav_menu(array(
+          'theme_location' => 'primary',
+          'menu_class'     => 'u3a-menu',
+          'container'      => false,
+          'depth'          => 2, // Enable dropdowns
+        ));
+      ?>
+      <a href="/donate" class="u3a-donate-btn">Donate Now</a>
+    </div>
   </nav>
 </header>
